55228a529cf444c5011e12f7912c75c542935080
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-22 00:44:12 +0300

    organizations

---
+++ Database/tables/org.table.sql
@@ -0,0 +1,36 @@
+
+
+USE STACKOVERFLOW;
+GO
+
+CREATE TABLE organizations
+(
+    id VARCHAR ( 255 ) PRIMARY KEY ,
+    name VARCHAR ( 255 ) NOT NULL ,
+    logo_url VARCHAR ( 255 ) NOT NULL ,
+    tag_id VARCHAR ( 255 ) NOT NULL ,
+    description VARCHAR ( 255 ) NOT NULL ,
+    created_at DATETIME NOT NULL DEFAULT GETDATE(),
+    updated_at DATETIME NOT NULL DEFAULT GETDATE(),
+    is_deleted BIT DEFAULT  0 ,
+    FOREIGN KEY (tag_id) REFERENCES tags(id),
+    CONSTRAINT FK_companies_tag_id_tags_id FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+
+
+USE STACKOVERFLOW;
+GO
+
+ALTER TABLE companies
+DROP CONSTRAINT FK_companies_tag_id_tags_id;
+
+ALTER TABLE companies
+ADD CONSTRAINT FK_companies_tag_id_tags_id FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE NO ACTION;
+
+ALTER TABLE companies
+ADD CONSTRAINT FK_companies_tag_id_tags_id FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE;
+
+
+
+

578f53effa174d8a8be535e31e32a44ca4337cbf
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-22 13:49:43 +0300

    user sp

---
+++ Database/storedProcedure/user/deleteUser.sql
@@ -0,0 +1,14 @@
+
+USE STACKOVERFLOW;
+GO
+
+
+
+CREATE OR ALTER PROCEDURE deleteUser
+@id VARCHAR ( 255 )
+AS
+BEGIN
+    SELECT * FROM users WHERE id = @id
+    DELETE FROM users WHERE id = @id
+   
+END
